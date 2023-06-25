efea1d0d2ce7fcb447c93f864f36145bb377bf4b
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-06-25 12:31:18 +0200

    added Screen library

---
+++ header/Screen.h
@@ -0,0 +1,16 @@
+#ifndef SCREEN_H_INCLUDED
+#define SCREEN_H_INCLUDED
+
+/*
+    Poesia dello Screen:
+        Se operazioni fighe tu vuoi fare
+        La libreria Screen dovrai utilizzare
+        Sovrascrivi e Cancella
+        Per rendere il tuo programma na cosa bella
+*/
+
+void ClearScreen(void);
+void SetCursorToHome(void);
+void ClearAndHome(void);
+
+#endif // SCREEN_H_INCLUDED
