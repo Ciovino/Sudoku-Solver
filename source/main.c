c6388f562697ad1ed218fd0f682ccb7563dd4ec2
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-06-25 13:45:48 +0200

    implemented PasteAndPrint operation

--- source/main.c
+++ source/main.c
@@ -1,10 +1,14 @@
 #include <conio.h>  // kbhit()
 #include <stdio.h>  // printf()
 #include <stdlib.h> // system()
+#include <string.h>
 
 #include "../header/Screen.h"
 
+#define SUDOKUDIGIT ".123456789"
+
 /* Function */
+void PrintGrid(int *sudoku_grid);
 void PrintMenu(void);
 void PasteAndPrint(void);
 void GenerateSudoku(void);
@@ -55,6 +59,23 @@ void PrintMenu(void)
 void PasteAndPrint(void)
 {
     printf("Paste And Print\n");
+
+    int sudoku_grid[81];
+    char buf[100];
+
+    printf("Paste an 81 digit string: ");
+    fgets(buf, 100, stdin);
+
+    int len = strlen(buf) - 1;
+    if (len != 81)
+        printf("Not enough digit. There are %d digits\n", len);
+    else {
+        for (int i = 0; i < 81; i++)
+            sudoku_grid[i] = buf[i] - 48;
+
+        PrintGrid(sudoku_grid);
+    }
+
     system("pause");
 }
 
@@ -62,6 +83,26 @@ void GenerateSudoku(void)
 {
     printf("Generate Sudoku\n");
     system("pause");
+}
+
+void PrintRow(int *sudoku_grid, int offset)
+{
+    for (int i = 0; i < 9; i++) {
+        if (i % 3 == 0) printf("|");
+        printf("%c", SUDOKUDIGIT[sudoku_grid[i + offset]]);
+        if (i % 3 != 2) printf(" ");
+    }
+    printf("|\n");
+}
+
+void PrintGrid(int *sudoku_grid)
+{
+    printf(".-----.-----.-----.\n");
+    for (int i = 0; i < 9; i++) {
+        PrintRow(sudoku_grid, i * 9);
+        if (i % 3 == 2 && i != 8) printf(":----- ----- -----:\n");
+    }
+    printf("'-----'-----'-----'\n");
 }
 
 // Takes a int value and choose the associates operation
