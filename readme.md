d5d02a3f4fee863494ddb2c350db46d7ac45f118
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-06-25 12:31:38 +0200

    simple title

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 Simple Sudoku Solver. Nothing less, Nothing more.
+
+# Create the .exe file
+gcc .\source\main.c .\source\Screen.c -o sudoku.exe
---
+++ source/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // printf
+
+#include "../header/Screen.h"
+
+int main(int argc, char **argv)
+{
+    ClearAndHome();
+    printf("Simple Sudoku Solver\n");
+
+    return 0;
+}
